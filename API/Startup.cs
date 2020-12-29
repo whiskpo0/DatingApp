57fc7825c3703a343fd957198e5ef243dc30c209
Adam M <whiskypo082@gmail.com>
2020-12-29 16:20:37 -0600

    End of section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.Data;
 using DatingApp.API.Extensions;
 using DatingApp.API.Interfaces;
+using DatingApp.API.Middleware;
 using DatingApp.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -45,10 +46,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+
+            app.UseMiddleware<ExceptionMiddleware>(); // our own middlewear for handeling errors
 
             app.UseHttpsRedirection();
 
